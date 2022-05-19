8a7e4cc45a231e094d0bf988adc10ef87ebcb9ef
MarioKyriacou <mkyriacou1@sheffield.ac.uk>
2022-05-19 15:24:37 +0100

    improved css navbar and forms ☺

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -390,8 +390,6 @@ function hideLoginInterface(room, userId) {
 function sendAxiosQuery(url, data) {
     axios.post(url, data)
         .then((dataR) => {// no need to JSON parse the result, as we are using
-            // we need to JSON stringify the object
-            document.getElementById('results').innerHTML = JSON.stringify(dataR.data);
         })
         .catch(function (response) {
             alert(response);
